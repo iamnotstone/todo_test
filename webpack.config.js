f994044a13510a45e272dd2831ddff95811c0b61
石头天工开物 <admin@example.com>
2017-06-16 19:10:47 +0800

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
                 test   : /\.js$/,
                 loader : 'babel-loader',
 		query: {
-          		presets: ['es2015', 'react']
+          		presets: ['es2015', 'react','stage-2']
         	}
             }
         ]
